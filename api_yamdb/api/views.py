4ee6ca7d84d05e9e761217e840deaa0ffc813f4c
Вахитов Рустам <therealrustam@yandex.ru>
2021-11-30 22:22:36 +0500

    Make view

--- api_yamdb/api/views.py
+++ api_yamdb/api/views.py
@@ -1,3 +1,22 @@
-from django.shortcuts import render
+from rest_framework import viewsets
+from rest_framework.generics import get_object_or_404
+from rest_framework.permissions import IsAuthenticated
 
-# Create your views here.
+from reviews.models import Comment, Review
+
+from .permissions import AuthorPermission
+from .serializers import CommentSerializer, GroupSerializer, PostSerializer
+
+
+class ReviewViewSet(viewsets.ModelViewSet):
+    serializer_class = ReviewSerializer
+    permission_classes = [AuthorPermission, IsAuthenticated]
+
+    def perform_create(self, serializer):
+        serializer.save(author=self.request.user)
+
+    def get_queryset(self):
+        title_id = self.kwargs.get('title_id')
+        title = get_object_or_404(Title, id=title_id)
+        new_queryset = Review.objects.filter(title_id=title.id)
+        return new_queryset
