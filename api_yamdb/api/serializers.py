f97d5bbb26a4db27029ac94ddc0c17113d3d516b
Вахитов Рустам <therealrustam@yandex.ru>
2021-12-03 22:20:17 +0500

    Add rating

--- api_yamdb/api/serializers.py
+++ api_yamdb/api/serializers.py
@@ -1,6 +1,28 @@
-from rest_framework import serializers
+import datetime as dt
+from django.db.models.aggregates import Avg
 
-from reviews.models import Comment, Review
+from rest_framework import serializers
+from reviews.models import Comment, Review, Title
+
+
+class TitleSerializer(serializers.ModelSerializer):
+    genre = serializers.SlugRelatedField(slug_field='slug', read_only=True)
+    category = serializers.SlugRelatedField(slug_field='slug', read_only=True)
+    rating = serializers.SerializerMethodField()
+
+    class Meta:
+        fields = ('name', 'year', 'description', 'genre', 'category',)
+        model = Title
+
+        def validate(self, data):
+            year = data.get('year')
+            if year > dt.date.today().year:
+                raise serializers.ValidationError(
+                    'Нельзя указать будущую дату'
+                )
+
+    def get_rating(self, obj):
+        return int(Review.objects.filter(id__title=obj.id).aggregate(Avg('score')).values())
 
 
 class ReviewSerializer(serializers.ModelSerializer):
